429d7f58f253b434f4cf677eb34960fc353c120e
bnros <b.ros92@gmail.com>
2022-12-13 06:32:20 -0800

    completed week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -23,7 +23,30 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+// PSEUDOCODE:
+
+// input: three objects containing different numbers
+// output: a new string designating whether if the provided numbers is divisible by three or not divisible by three.
+
+// process:
+// declare a function named "divByThree"
+// after declaring function, in the paramter add "object"
+// using dot notation, to access the correct object
+// create a return statement using string interpolation for if the provided number is divisble by three
+// create another return statement using string interpolation if the provided number is NOT divisible by three
+
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+// RED:
+describe("divByThree", () => {
+    it("calculates if the given number is divisble by three", () => {
+        expect(divByThree(object1)).toEqual("15 is divisible by three.")
+        expect(divByThree(object2)).toEqual("0 is divisible by three.")
+        expect(divByThree(object3)).toEqual("-7 is NOT divisible by three.")
+    })
+})
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -34,9 +57,37 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
+// GREEN:
+const divByThree = (object) => {
+    if (object.number % 3 === 0){
+        return `${object.number} is divisible by three.`
+    } else if (object.number % 3 !== 0){
+        return `${object.number} is NOT divisible by three.`
+    }
+}
+
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
+// PSEUDOCODE:
+
+// input: an array with all lower case values
+// output: a new array with the first letter of each value capitilized
+
+// process:
+// declare a function called "capitlizedArray"
+// after declaring function, in the paramter add "array"
+// use the functions of .map() in the return statement to bring forth a new array with the same values, but mofidied
+// within the return statement, create another return statement using the functions of charAt(0) to specify to return a specific character position within the string, followed by .toUpperCase to convert all the letters in the string to uppercase, and ending with + slice(1) to designate what position we want it to start and end at.
+
 // a) Create a test with expect statements for each of the variables provided.
+
+// RED:
+describe("capitalizedArray", () => {
+    it("capitlizes the first letter of the values in the array", () =>{
+        expect(capitilizedArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(capitilizedArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"]) 
+    })
+})
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -45,9 +96,37 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+// GREEN:
+const capitilizedArray = (array) => {
+    return array.map(value => {
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    })
+}
+
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
+// PSEUDOCODE:
+
+// input: three strings containing vowels and its placements
+// output: the vowel placement and its index value of the provided strings
+
+// process:
+// declare a function called "indexOfFirstVowl"
+// after declaring function, in the paramter add "string"
+// using search() automatically returns the index position of the first match
+// hardcode the vowels within search() so that it'll only return a, e, i, o, u, and sometimes y
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+// RED:
+describe("indexOfFirstVowel", () => {
+    it("returns the index of the first vowel", () =>{
+        expect(indexOfFirstVowel(vowelTester1)).toEqual(1)
+        expect(indexOfFirstVowel(vowelTester2)).toEqual(0)
+        expect(indexOfFirstVowel(vowelTester3)).toEqual(2)
+    })
+})
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -57,3 +136,8 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+
+// GREEN:
+const indexOfFirstVowel = (string) => {
+    return string.search(/[a,e,i,o,u,y]/)
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,36 +8,49 @@
 // --------------------1) What will this log?
 
 const cohort = "Hotel 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I believe that this will log ["H", 
+// "o", "t", "e", l" "", "2", "0", "2", "2"].
+// b) Verify and explain: After running it through terminal, it logged as I expected to. The results came back with [
+//   'H', 'o', 't', 'e',
+//   'l', ' ', '2', '0',
+//   '2', '2'
+// ]
+// This is because .split() is a mutator that takes in a string, and returns a new array with each letter in the string as a new value within the new array.
+
+
+
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I believe that this function should log "Hello, LEARN Student!".
+// b) Verify and explain: After running it through terminal, it returned a message saying "undefined". I did not observe closely enough to realize that in our function, there was no return given, therefore it did not work. In order for it to return a message, we would have to add "return" before the string interpolation.
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This should return a new array in terminal with the values [8, 10, 12, 14, 16].
+// b) Verify and explain: After verifying the console.log within temrinal, it returned:
+// [ 8, 10, 12, 14, 16 ]
+// My answer turned out to be correct because with using the method of .map(), we are telling it to return a new array while iterating and passing the function to each value of the previous array.
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Because of the .filter() method, this should log [11, 13, 15] when we run it through terminal.
+// b) Verify and explain: When we log the array above in terminal, it returns a new array as expected with the following:
+// [ 11, 13, 15 ]
+// This is because with the .filter(), it returns a new array based on the condition we give it. In this cas, it was told to pass on "number % 2 !== 0" thus, returning only odd numbers from the original array.
 
 // --------------------5) What will this log?
 
@@ -47,10 +60,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I'm confident that this will log "JavaScript".
+// b) Verify and explain: After verifying it within terminal, I was correct with my answer because within the object, we have a key called "languages", and within that key there are two values, called JavaScript and Ruby. To call upon it, as we did in console.log, we have to use dot notation to extract the value, and because we followed with bracket notation, it extracted and return the first value in the zero index, corresponding to JavaScript.
 
 // --------------------STRETCH: What will this log?
 
@@ -62,7 +75,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
 // a) Your answer:
 // b) Verify and explain:
